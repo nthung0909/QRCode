1acef8248ff5e1d23aaf542a53a1b889cf299637
ngthung0909 <nthung0909>
2024-05-11 10:35:29 +0700

    add git ignore

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 
 app.get('/',async(req,res)=>{
     let img='';
-    let qr= await QRCode.toDataURL('[DC9-A3]U42-11/2/0<=>[DC9-A17]U34-Core 59,60<=>[P KT Trệt 1B-R2]U34-Core 59,60<=>[P KT Trệt 1A-R6]U40-Core 90,91<=>[DC7-P10]U25-0/10');
+    let qr= await QRCode.toDataURL('https://github.com/nthung0909/QRCode', {
+        color: {
+            dark: '#FF90BC',
+            light: '#0000'
+        }
+    });
     img = `<image src= " `+qr+ `" />`
     return res.send(img);
 });
